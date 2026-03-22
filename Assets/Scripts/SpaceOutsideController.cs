88a39d6fd491d8293b0072ca4d27a88c57f6b307
BEB024 <ilabib2004@gmail.com>
2026-03-22 16:34:30 +0200

    Space Scraper - User Interface and Game Audio

--- Assets/Scripts/SpaceOutsideController.cs
+++ Assets/Scripts/SpaceOutsideController.cs
@@ -12,6 +12,8 @@ public class SpaceOutsideController : MonoBehaviour
     public float forwardSpeed;
     public float sideSpeed;
 
+    private bool wasOn;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +28,19 @@ public class SpaceOutsideController : MonoBehaviour
 
         Vector3 velocity = new Vector3(sideVelocity, 0, forwardVelocity);
         transform.position += velocity * Time.deltaTime;
+
+        if (lever.value != wasOn)
+        {
+            if (lever.value)
+            {
+                AudioManager.instance.Play("Engine");
+            }
+            else
+            {
+                AudioManager.instance.Stop("Engine");
+            }
+        }
+
+        wasOn = lever.value;
     }
 }
